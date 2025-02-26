70660a087189ef3cb2b7af76fb7130efe90e7b32
MrBlack-FT <132612066+MrBlack-FT@users.noreply.github.com>
2025-02-26 18:03:08 +0100

    Képkockaszám korlátozás javítás
    Foltozási megjegyzések:
      - Javítva:
        -A képkockaszám minden platformon 60 FPS-re volt korlátozva. [PC-n mostantól nincs korlátlan]

--- Assets/Scripts/Entities/Spawners/Level3Spawner.cs
+++ Assets/Scripts/Entities/Spawners/Level3Spawner.cs
@@ -15,10 +15,6 @@ public class Level3Spawner : MonoBehaviour
     #endregion
     private bool finishable = false;
 
-    void Awake(){
-        Application.targetFrameRate = 60;
-    }
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
